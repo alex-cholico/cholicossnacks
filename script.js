7720950992c00ff60d7e8f19d51253d79e7f6b11
alex-cholico <133170913+alex-cholico@users.noreply.github.com>
2023-05-10 21:12:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+  }, 2000);
+});
