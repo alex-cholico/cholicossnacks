81066b541c4d3dff3cbdede9853c885f14d3dbd0
alex-cholico <133170913+alex-cholico@users.noreply.github.com>
2023-05-12 08:04:05 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden";
-  }, 2000);
-});
