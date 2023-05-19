67c0e6a8d6e31f1dd87af6153d2a4381e976ec44
alex-cholico <133170913+alex-cholico@users.noreply.github.com>
2023-05-19 08:32:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ window.addEventListener("load", function () {
     loader.className += " hidden";
   }, 2000);
 });
+const sliderContainer = document.querySelector('.slider-container');
+const sliderImages = document.querySelectorAll('.slider-image');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+let currentIndex = 0;
+
+arrowLeft.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + sliderImages.length) % sliderImages.length;
+  sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
+
+arrowRight.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % sliderImages.length;
+  sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
